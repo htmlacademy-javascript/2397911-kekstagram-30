acdff03f2c70aa4b232bc036d4abcf9315528ed8
Nastya880 <oidmakeevy@gmail.com>
2023-11-26 22:44:48 +0300

    Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ import { renderGallery } from './gallery.js';
 import './form.js';
 import { loadPictures } from './api.js';
 import { showErrorMessage } from './util.js';
+import { initFilter } from './filters.js';
 
 async function bootstrap() {
   try {
     const pictures = await loadPictures();
     renderGallery(pictures);
+    initFilter();
   } catch (error) {
     // console.error(error);
     showErrorMessage();
@@ -14,3 +16,12 @@ async function bootstrap() {
 }
 
 bootstrap();
+
+// try {
+//   // const data = await loadPictures();
+//   const debouncedRenderGallery = debounce(renderGallery);
+//   // initFilter(data, debouncedRenderGallery);
+//   // renderGallery(debouncedRepaint());
+// } catch {
+//   showErrorMessage();
+// }
